dd4bcb255a845d5be89dd2feb40bec34fd50e451
lucasmiguez1 <102496457+lucasmiguez1@users.noreply.github.com>
2023-07-19 00:07:00 -0300

    Update EXTRACT_HP_API.py

--- EXTRACT_HP_API.py
+++ EXTRACT_HP_API.py
@@ -1,10 +1,13 @@
+#Librería necesaria para extraer los datos
 import requests
 
+#Función para chequear el status de la API y extraer sus datos
 def obtener_datos_api(url):
     try:
+        #Llamada a la API
         response = requests.get(url)
 
-        # Verificar si la solicitud fue exitosa (código de estado 200)
+        #Checkear si la llamada fue exitosa
         if response.status_code == 200:
             datos = response.json()
             return datos
@@ -15,17 +18,19 @@ def obtener_datos_api(url):
         print("Error en la solicitud:", e)
         return None
 
+#URL de la API
 url_api = "https://hp-api.onrender.com/api/characters/staff"
+
+#Invocación de la función para extraer datos
 datos_api = obtener_datos_api(url_api)
 
+#Condicional para chequear que se extrayeron los datos
 if datos_api:
-        print("Datos obtenidos:")
-        print(datos_api)
+        print(f"Datos obtenidos: {datos_api}")
 
-        # Obtener los campos ordenados en una lista
+        #Obtener los campos ordenados en una lista
         campos_ordenados = list(datos_api[0].keys())
 
-        print("Campos ordenados:")
-        print(campos_ordenados)
+        print(f"Campos ordenados: {campos_ordenados}")
 else:
-        print("No se pudieron obtener los datos de la API.")
+        print("No se pudieron obtener los datos de la API")
